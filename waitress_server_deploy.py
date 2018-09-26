687e07171b0644970216d5a216bad1403cbcecaf
macsag <maciejsagata@gmail.com>
2018-09-26 18:49:14 +0200

    0.3.1 better logging to file

--- waitress_server_deploy.py
+++ waitress_server_deploy.py
@@ -1,8 +1,21 @@
 from waitress import serve
-from api_morepath import *
+import api_morepath
+import logging
+import sys
+from base_url_config import BASE_URL
 
-logging.root.addHandler(logging.FileHandler('log_marc_api.txt', encoding='utf-8'))
-logging.root.setLevel(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s',
+    datefmt="%H:%M:%S",
+    stream=sys.stdout)
+
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s')
+
+root_fh = logging.FileHandler('log_marc_api.log', encoding='utf-8')
+root_fh.setLevel(logging.INFO)
+root_fh.setFormatter(formatter)
+logging.root.addHandler(root_fh)
 
 
-serve(App(), listen=BASE_URL)
+serve(api_morepath.App(), listen=BASE_URL)
