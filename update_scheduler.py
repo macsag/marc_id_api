2db29b483383964aa2937b1bbb2282e6b81e8589
macsag <maciejsagata@gmail.com>
2018-09-26 18:48:42 +0200

    0.3.1 add bib_update

--- update_scheduler.py
+++ update_scheduler.py
@@ -6,10 +6,13 @@ from base_url_config import BASE_URL
 def do_auth_update():
     requests.get('http://{}/update/authorities'.format(BASE_URL))
 
+def do_bib_update():
+    requests.get('http://{}/update/bibs'.format(BASE_URL))
 
 # set update scheduler
-schedule.every().day.at("17:26").do(do_auth_update)
+schedule.every().day.at("09:15").do(do_auth_update)
+schedule.every().day.at("12:45").do(do_auth_update)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(30)
