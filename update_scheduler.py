8c9fab4de3aaa4c224527cf1b5b0be2fd8131cca
macsag <maciejsagata@gmail.com>
2018-11-29 22:48:11 +0100

    added catching exceptions

--- update_scheduler.py
+++ update_scheduler.py
@@ -2,15 +2,30 @@ import schedule
 import requests
 import time
 from base_url_config import BASE_URL
+import functools
 
+def catch_exceptions(job_func, cancel_on_failure=False):
+    @functools.wraps(job_func)
+    def wrapper(*args, **kwargs):
+        try:
+            return job_func(*args, **kwargs)
+        except:
+            import traceback
+            print(traceback.format_exc())
+            if cancel_on_failure:
+                return schedule.CancelJob
+    return wrapper
+
+@catch_exceptions
 def do_auth_update():
     requests.get('http://{}/update/authorities'.format(BASE_URL))
 
+@catch_exceptions
 def do_bib_update():
     requests.get('http://{}/update/bibs'.format(BASE_URL))
 
 # set update scheduler
-schedule.every().day.at("10:15").do(do_auth_update)
+schedule.every().day.at("22:46").do(do_auth_update)
 schedule.every().day.at("10:30").do(do_bib_update)
 
 while True:
